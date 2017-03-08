8e8fd306007cb6e6826fc79dc0af628433632c31
JL <jeanlouis02300@gmail.com>
2017-03-08 12:24:19 +0100

    maj

--- lectureweb.py
+++ lectureweb.py
@@ -1,7 +1,14 @@
-import urllib, os, sys
-import urllib2
-from bs4 import BeautifulSoup
+import bs4, urllib
 
-response = urllib2.urlopen('https://twitter.com/KittingKitten')
-html = response.read()
-soup = BeautifulSoup(html)
+URL="https://twitter.com/morpheeDgami"
+
+page = urllib.urlopen(URL)
+data=page.read().decode('UTF-8')
+soup=bs4.BeautifulSoup(data, 'html.parser')
+
+recup=soup.findAll(attrs={"class":"js-tweet-text-container"})
+
+tweet=[]
+for i in recup:
+
+print (base64.b64decode(tweet[0]))
